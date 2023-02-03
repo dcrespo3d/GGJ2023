5b86041c5d50b1bdef82dda0a934a05aee8bc8ec
guipagon <guipadgon@gmail.com>
2023-02-03 20:56:10 +0100

    DASH DE MIERDA

--- Player.gd
+++ Player.gd
@@ -12,12 +12,24 @@ func _ready():
 
 export var walkspeed = 30
 export var dashspeed = 60
+export var dashduration = 20
+var dashstale = 1
 var lookleft = false
 enum {NORMAL, SUCK, BUSY, HIT, DEAD}
 var state = NORMAL
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	match (state):
+		NORMAL: process_normal(delta)
+		SUCK: process_suck(delta)
+		BUSY: process_busy(delta)
+		HIT: process_hit(delta)
+		DEAD: process_dead(delta)
+		
+	dashstale = dashstale + 0.001
+		
+func process_normal(delta):
 	if Input.is_action_pressed("ui_left"):
 		position.x -= walkspeed * delta
 		lookleft = true
@@ -28,28 +40,33 @@ func _process(delta):
 		position.y -= walkspeed * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += walkspeed * delta
-
-	match (state):
-		NORMAL: process_normal()
-		SUCK: process_suck()
-		BUSY: process_busy()
-		HIT: process_hit()
-		DEAD: process_dead()
-		
-		
-func process_normal():
+	
+	if Input.is_action_just_released("dash_key"):
+			dash(delta)
+	
+func process_suck(delta):
 	return
 	
-func process_suck():
+func process_busy(delta):
 	return
 	
-
-func process_busy():
+func process_hit(delta):
 	return
 	
-func process_hit():
-	return
-	
-func process_dead():
+func process_dead(delta):
 	return
 
+
+func dash(delta):
+	state = BUSY
+	var dashlength = dashduration
+	if lookleft:
+		while (dashlength * dashstale > 0):
+			position.x -= dashspeed * delta
+			dashlength = dashlength - 1
+	else:
+		while (dashlength * dashstale > 0):
+			position.x += dashspeed * delta
+			dashlength = dashlength - 1
+	state = NORMAL
+	return
