463aa5c18989fc5dfbacad6bddd1a7176b9518a7
David Crespo <dcrespo3d@gmail.com>
2023-02-03 20:53:04 +0100

    colliders: suelo, laterales, y caminar

--- Player.gd
+++ Player.gd
@@ -10,46 +10,54 @@ extends KinematicBody2D
 func _ready():
 	pass # Replace with function body.
 
-export var walkspeed = 30
+export var walkspeed = 300
 export var dashspeed = 60
+export var jumpspeed = -500
+export var fallacc = 1000
+var velocity = Vector2.ZERO	
+		
+
+
 var lookleft = false
 enum {NORMAL, SUCK, BUSY, HIT, DEAD}
 var state = NORMAL
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	match (state):
+		NORMAL: process_normal(delta)
+		SUCK: process_suck(delta)
+		BUSY: process_busy(delta)
+		HIT: process_hit(delta)
+		DEAD: process_dead(delta)
+		
+func process_normal(delta):
+	velocity.x = 0
 	if Input.is_action_pressed("ui_left"):
-		position.x -= walkspeed * delta
+		velocity.x = -walkspeed
 		lookleft = true
 	if Input.is_action_pressed("ui_right"):
-		position.x += walkspeed * delta
+		velocity.x = walkspeed
 		lookleft = false
-	if Input.is_action_pressed("ui_up"):
-		position.y -= walkspeed * delta
-	if Input.is_action_pressed("ui_down"):
-		position.y += walkspeed * delta
-
-	match (state):
-		NORMAL: process_normal()
-		SUCK: process_suck()
-		BUSY: process_busy()
-		HIT: process_hit()
-		DEAD: process_dead()
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = jumpspeed
 		
+	velocity.y += fallacc * delta
 		
-func process_normal():
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
 	return
 	
-func process_suck():
+func process_suck(delta):
 	return
 	
 
-func process_busy():
+func process_busy(delta):
 	return
 	
-func process_hit():
+func process_hit(delta):
 	return
 	
-func process_dead():
+func process_dead(delta):
 	return
 
