9fe65c22ecc10d13cf201e9708924fc72fe97bf0
alvarocg-a <calvoalvaro12@gmail.com>
2023-02-04 17:58:13 +0100

    deadscene

--- Gea.gd
+++ Gea.gd
@@ -15,9 +15,9 @@ func getType():
 func _takeHit(damage):
 	if currentHealth > 0:
 		currentHealth -= damage	
-#func _process(delta):
-	##if currentHealth <= 0:
-		#get_tree().change_scene_to(nivelGameOver)
+func _process(delta):
+	if currentHealth <= 0:
+		get_tree().get_root().get_node("EscenaMain/Viewport/GameOver").visible=true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
